1464aa35d87e543a1c170a886ce88ddb647e4301
hila464 <hila@softimize.co>
2019-09-04 00:01:01 +0300

    minor pom fixes & lib

--- server/src/main/java/MainApplication.java
+++ server/src/main/java/MainApplication.java
@@ -1,5 +1,3 @@
-package src.main.java;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
