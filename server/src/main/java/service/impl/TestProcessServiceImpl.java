dbbe9ac46847348438560788c6c0f4609eb1d696
hila464 <hila@softimize.co>
2019-09-03 16:33:36 +0300

    minor pom fixes

--- server/src/main/java/service/impl/TestProcessServiceImpl.java
+++ server/src/main/java/service/impl/TestProcessServiceImpl.java
@@ -2,6 +2,7 @@ package src.main.java.service.impl;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import src.main.java.service.ITestProcessService;
 
@@ -10,6 +11,7 @@ import java.io.IOException;
 
 
 @Service
+@Scope("singleton") //This is the default
 public class TestProcessServiceImpl implements ITestProcessService {
     public void addTest(String filePath) throws IOException {
         PDDocument document = PDDocument.load(new File(filePath));
