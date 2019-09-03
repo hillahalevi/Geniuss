d45746bc3a7f1cdd895ca9d5baac95a111ce2850
hila464 <hila@softimize.co>
2019-09-03 12:52:48 +0300

    Initial commit - project set up

---
+++ src/service/ITestProcessService.java
@@ -0,0 +1,10 @@
+package service;
+
+import java.io.IOException;
+
+public interface ITestProcessService {
+
+    void addTest(String filePath) throws IOException;
+
+
+}
---
+++ src/service/impl/TestProcessServiceImpl.java
@@ -0,0 +1,21 @@
+package service.impl;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+import service.ITestProcessService;
+
+public class TestProcessServiceImpl implements ITestProcessService {
+    @Override
+    public void addTest(String filePath) throws IOException {
+        PDDocument document = PDDocument.load(new File(filePath));
+        if (!document.isEncrypted()) {
+            PDFTextStripper stripper = new PDFTextStripper();
+            String text = stripper.getText(document);
+            System.out.println("Text:" + text);
+        }
+        document.close();
+    }
+}
